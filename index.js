c89ed53be84ec041b12de7582f999fe1f20f718a
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-04 12:33:18 +0700

    use environment variable when available
    See: https://gitlab.com/gnachman/iterm2/issues/4465

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ let version;
 
 module.exports = () => {
 	if (!version) {
-		const fp = path.join(appPath.sync('iTerm'), 'Contents/Info.plist');
-		version = plist.parse(fs.readFileSync(fp, 'utf8')).CFBundleVersion;
+		if (process.env.TERM_PROGRAM_VERSION) {
+			version = process.env.TERM_PROGRAM_VERSION;
+		} else {
+			const fp = path.join(appPath.sync('iTerm'), 'Contents/Info.plist');
+			version = plist.parse(fs.readFileSync(fp, 'utf8')).CFBundleVersion;
+		}
 	}
 
 	return version;
